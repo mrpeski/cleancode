43065ff7c664a46021e4fe749ce550229d74f26c
Olayinka Ogunwemimo <mimoyinka@gmail.com>
2021-05-16 15:22:24 +0100

    Bigger Selection
    Even though this is close to a violation of the rule about mental
    mapping, it seems appropriate in this case. Notice, once you know the
    meaning, your eyes glide across that string and you can quickly
    interpret the results. Reading the test becomes almost a pleasure.
    Just take a look at Listing 9-5 and see how easy it is to understand
    these tests.

--- EnvironmentControllerTest.java
+++ EnvironmentControllerTest.java
@@ -1,5 +1,22 @@
 @Test
-public void turnOnLoTempAlarmAtThreshold() throws Exception              wayTooCold();
-       assertEquals("HBchL", hw.getState());
+   public void turnOnCoolerAndBlowerIfTooHot() throws Exception {
+     tooHot();
+     assertEquals("hBChl", hw.getState());
+   }
 
-}
+   @Test
+   public void turnOnHeaterAndBlowerIfTooCold() throws Exception {
+     tooCold();
+     assertEquals("HBchl", hw.getState());
+   }
+
+   @Test
+   public void turnOnHiTempAlarmAtThreshold() throws Exception {
+     wayTooHot();
+     assertEquals("hBCHl", hw.getState());
+   }
+   @Test
+   public void turnOnLoTempAlarmAtThreshold() throws Exception {
+     wayTooCold();
+     assertEquals("HBchL", hw.getState());
+   }
