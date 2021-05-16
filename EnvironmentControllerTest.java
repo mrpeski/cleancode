4cf355a6d1860d865a1104a55e6c8072304106cf
Olayinka Ogunwemimo <mimoyinka@gmail.com>
2021-05-16 15:16:50 +0100

    Refactored
    Of course I hid the detail of the tic function by creating a wayTooCold
    function. But the thing to note is the strange string in the
    assertEquals. Upper case means “on,” lower case means “off,” and the
    letters are always in the following order: {heater, blower, cooler,
    hi-temp-alarm, lo-temp-alarm}.

--- EnvironmentControllerTest.java
+++ EnvironmentControllerTest.java
@@ -1,10 +1,5 @@
 @Test
-public void turnOnLoTempAlarmAtThreashold() throws Exception {
-       hw.setTemp(WAY_TOO_COLD);
-       controller.tic();
-       assertTrue(hw.heaterState());
-       assertTrue(hw.blowerState());
-       assertFalse(hw.coolerState());
-       assertFalse(hw.hiTempAlarm());
-       assertTrue(hw.loTempAlarm());
+public void turnOnLoTempAlarmAtThreshold() throws Exception              wayTooCold();
+       assertEquals("HBchL", hw.getState());
+
 }
